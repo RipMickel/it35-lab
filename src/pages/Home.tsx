c0fd5e38fa7e8622d7cb1ac67721635b831fa72e
RipMickel <gaudicosmickelangelo@gmail.com>
2025-02-20 18:53:50 +0800

    Added button for details

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,24 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader>
           <IonToolbar>
-          <IonButtons slot='start'>
+            <IonButtons slot='start'>
               <IonMenuButton ></IonMenuButton>
             </IonButtons>
-            <IonTitle>Home</IonTitle>          </IonToolbar>
+            <IonTitle>Home</IonTitle>
+          </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+        <IonContent fullscreen>
+          <IonButton routerLink="/it35-lab/app/home/details" expand="full">
+              Details
+          </IonButton>
+        </IonContent>
+      </IonPage>
   );
 };
 
