5460ef8781ba11a09a449a87fc6d1b70a7697317
RipMickel <gaudicosmickelangelo@gmail.com>
2025-02-27 08:37:35 +0800

    added some components to my favorites.tsx

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -1,12 +1,21 @@
 import { 
+  IonAvatar,
     IonButtons,
       IonContent, 
       IonHeader, 
+      IonIcon, 
+      IonItem, 
+      IonItemOption, 
+      IonItemOptions, 
+      IonItemSliding, 
+      IonLabel, 
+      IonList, 
       IonMenuButton, 
       IonPage, 
       IonTitle, 
       IonToolbar 
   } from '@ionic/react';
+import { call, pin, share, trash } from 'ionicons/icons';
   
   const Favorites: React.FC = () => {
     return (
@@ -19,17 +28,92 @@ import {
             <IonTitle>Favorites</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Favorites
-        </div>
+        <IonContent fullscreen color = "light">
+       <IonList inset={true}>
+                 <IonItemSliding>
+                   <IonItem button={true}>
+                     <IonAvatar aria-hidden="true" slot="start">
+                       <img alt="" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                     </IonAvatar>
+                     <IonLabel>Zoro</IonLabel>
+                     <IonIcon aria-hidden="true" icon={call} slot="start"></IonIcon>
+                   </IonItem>
+                   <IonItemOptions slot="end">
+                     <IonItemOption color="warning">
+                       <IonIcon slot="icon-only" icon={pin}></IonIcon>
+                     </IonItemOption>
+                     <IonItemOption color="tertiary">
+                       <IonIcon slot="icon-only" icon={share}></IonIcon>
+                     </IonItemOption>
+                     <IonItemOption color="danger" expandable={true}>
+                       <IonIcon slot="icon-only" icon={trash}></IonIcon>
+                     </IonItemOption>
+                   </IonItemOptions>
+                 </IonItemSliding>
+       
+                 <IonItemSliding>
+                   <IonItem button={true}>
+                     <IonAvatar aria-hidden="true" slot="start">
+                       <img alt="" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                     </IonAvatar>
+                     <IonLabel>Luffy</IonLabel>
+                     <IonIcon aria-hidden="true" icon={call} slot="start"></IonIcon>
+                   </IonItem>
+                   <IonItemOptions slot="end">
+                     <IonItemOption color="warning">
+                       <IonIcon slot="icon-only" icon={pin}></IonIcon>
+                     </IonItemOption>
+                     <IonItemOption color="tertiary">
+                       <IonIcon slot="icon-only" icon={share}></IonIcon>
+                     </IonItemOption>
+                     <IonItemOption color="danger" expandable={true}>
+                       <IonIcon slot="icon-only" icon={trash}></IonIcon>
+                     </IonItemOption>
+                   </IonItemOptions>
+                 </IonItemSliding>
+       
+                 <IonItemSliding>
+                   <IonItem button={true}>
+                     <IonAvatar aria-hidden="true" slot="start">
+                       <img alt="" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                     </IonAvatar>
+                     <IonLabel>Shanks</IonLabel>
+                     <IonIcon aria-hidden="true" icon={call} slot="start"></IonIcon>
+                   </IonItem>
+                   <IonItemOptions slot="end">
+                     <IonItemOption color="warning">
+                       <IonIcon slot="icon-only" icon={pin}></IonIcon>
+                     </IonItemOption>
+                     <IonItemOption color="tertiary">
+                       <IonIcon slot="icon-only" icon={share}></IonIcon>
+                     </IonItemOption>
+                     <IonItemOption color="danger" expandable={true}>
+                       <IonIcon slot="icon-only" icon={trash}></IonIcon>
+                     </IonItemOption>
+                   </IonItemOptions>
+                 </IonItemSliding>
+       
+                 <IonItemSliding>
+                   <IonItem button={true}>
+                     <IonAvatar aria-hidden="true" slot="start">
+                       <img alt="" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                     </IonAvatar>
+                     <IonLabel>Gol D Roger</IonLabel>
+                     <IonIcon aria-hidden="true" icon={call} slot="start"></IonIcon>
+                   </IonItem>
+                   <IonItemOptions slot="end">
+                     <IonItemOption color="warning">
+                       <IonIcon slot="icon-only" icon={pin}></IonIcon>
+                     </IonItemOption>
+                     <IonItemOption color="tertiary">
+                       <IonIcon slot="icon-only" icon={share}></IonIcon>
+                     </IonItemOption>
+                     <IonItemOption color="danger" expandable={true}>
+                       <IonIcon slot="icon-only" icon={trash}></IonIcon>
+                     </IonItemOption>
+                   </IonItemOptions>
+                 </IonItemSliding>
+               </IonList>
         </IonContent>
       </IonPage>
     );
