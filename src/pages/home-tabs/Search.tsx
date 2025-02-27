7c8213def9351a84645868fa0936fbc0e3dd679c
RipMickel <gaudicosmickelangelo@gmail.com>
2025-02-27 08:40:38 +0800

    Added some components to Search.tsx

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,9 +1,15 @@
 import { 
+  IonAvatar,
     IonButtons,
       IonContent, 
       IonHeader, 
+      IonImg, 
+      IonItem, 
+      IonLabel, 
+      IonList, 
       IonMenuButton, 
       IonPage, 
+      IonSearchbar, 
       IonTitle, 
       IonToolbar 
   } from '@ionic/react';
@@ -20,17 +26,46 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Search
-        </div>
-        </IonContent>
+        <IonSearchbar placeholder="Search"></IonSearchbar>
+            <IonList>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>Connor Smith</h2>
+                  <p>Sales Rep</p>
+                </IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>Daniel Smith</h2>
+                  <p>Product Designer</p>
+                </IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>Greg Smith</h2>
+                  <p>Director of Operations</p>
+                </IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>Zoey Smith</h2>
+                  <p>CEO</p>
+                </IonLabel>
+              </IonItem>
+            </IonList>
+          </IonContent>
       </IonPage>
     );
   };
