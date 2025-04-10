f7987b5b7cfbb12d2752a3c2330e5db5095168d3
RipMickel <gaudicosmickelangelo@gmail.com>
2025-04-10 21:10:28 +0800

    Updates

--- assets/ios.transition-legacy-DonYHXMA.js
+++ assets/ios.transition-legacy-DonYHXMA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DNJ4QOof.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-C67yIlfY.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
