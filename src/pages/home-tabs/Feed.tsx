254076c13a8508c3f2cc49d785d14ef302c66584
RipMickel <gaudicosmickelangelo@gmail.com>
2025-04-24 00:40:26 +0800

    Hotfix: feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -22,17 +22,7 @@ const Feed: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <FeedContainer />
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100%',
-        }}
-      >
-        Feed
-      </div>
-      <FeedContainer />
+      
       </IonContent>
     </IonPage>
   );
