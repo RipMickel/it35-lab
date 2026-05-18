123e5e340c575471dbfd1b9ccf1f53d7c8d221a9
RipMickel <gaudicosmickelangelo@gmail.com>
2026-05-18 11:27:16 +0800

    Updates

--- assets/md.transition-legacy-ChW55pRu.js
+++ assets/md.transition-legacy-ChW55pRu.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CLCm9d3F.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-O7HF2NGb.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DUT-81Zi.js
+++ assets/swipe-back-legacy-DUT-81Zi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CLCm9d3F.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-O7HF2NGb.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
